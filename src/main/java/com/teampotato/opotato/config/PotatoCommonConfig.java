b5e3359df223b0ce8b04f6e0daa8fd47057ee55d
Kasualix <670067575@qq.com>
2023-03-07 23:24:35 +0800

    add fence jumper

--- src/main/java/com/teampotato/opotato/config/PotatoCommonConfig.java
+++ src/main/java/com/teampotato/opotato/config/PotatoCommonConfig.java
@@ -5,11 +5,19 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class PotatoCommonConfig {
     public static final ForgeConfigSpec COMMON_CONFIG;
     public static final ForgeConfigSpec.BooleanValue ALTERNATE_CURRENT_DEBUG_MODE;
+    public static final ForgeConfigSpec.BooleanValue ENABLE_FENCE_JUMP;
 
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
         CONFIG_BUILDER.comment("Opotato").push("alternatecurrent");
         ALTERNATE_CURRENT_DEBUG_MODE = CONFIG_BUILDER.define("enable alternate current debug mode", false);
+        ENABLE_FENCE_JUMP = CONFIG_BUILDER
+                .comment(
+                        "Compared with the 'Jump Over Fences' mod, the fence jumping function of this mod is better optimized.",
+                        "I had experienced crash with 'Jump Over Fences' before, so I don't want to use it again and wrote this instead.",
+                        "This is disabled by default, because this is not optimization & don't fit well with the concept of Opotato."
+                )
+                .define("enable fence jump", false);
         CONFIG_BUILDER.pop();
         COMMON_CONFIG = CONFIG_BUILDER.build();
     }
---
+++ src/main/java/com/teampotato/opotato/event/FenceJumperEvent.java
@@ -0,0 +1,44 @@
+package com.teampotato.opotato.event;
+
+import com.teampotato.opotato.config.PotatoCommonConfig;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.level.block.WallBlock;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = "opotato", value = {Dist.CLIENT})
+public class FenceJumperEvent {
+
+    private static Block getBlock(Level world, BlockPos pos) {
+        return world.getBlockState(pos).getBlock();
+    }
+
+    public static boolean isPlayerNextToFence(LocalPlayer player) {
+        double x = player.getX() - 1.0D;
+        double y = player.getY();
+        double z = player.getZ() - 1.0D;
+        for(int i = 0; i < 9; ++i) {
+            int row = i / 3;
+            int col = i % 3;
+            if (row == 1 && col == 1) continue;
+            Block block = getBlock(player.level, new BlockPos(x + (double)col, y, z + (double)row));
+            if (block instanceof FenceBlock || block instanceof WallBlock) return true;
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public static void playerJumpFence(LivingEvent.LivingJumpEvent event) {
+        if (!PotatoCommonConfig.ENABLE_FENCE_JUMP.get() || !(event.getEntity() instanceof LocalPlayer)) return;
+        LocalPlayer player = (LocalPlayer) event.getEntity();
+        if (isPlayerNextToFence(player) && player.input.jumping) {
+            player.setDeltaMovement(player.getDeltaMovement().add(0.0D, 0.05D, 0.0D));
+        }
+    }
+}
