71c7f712a53f1f36c8d8e6ec0a1b022472138920
Kasualix <670067575@qq.com>
2023-03-10 16:09:50 +0800

    fix name

--- src/main/java/com/teampotato/opotato/mixin/nec/client/EntityRenderDispatcherMixin.java
+++ src/main/java/com/teampotato/opotato/mixin/nec/client/EntityRenderDispatcherMixin.java
@@ -1,6 +1,7 @@
 package com.teampotato.opotato.mixin.nec.client;
 
 import com.teampotato.opotato.util.nec.mixinhandlers.EntryPointCatcher;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -8,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.util.Iterator;
 
 @Mixin(EntityRenderDispatcher.class)
-public class EntityRenderDispatcher {
+public class EntityRenderDispatcherMixin {
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Iterator;hasNext()Z"))
     private boolean redirectHasNext(Iterator iterator) {
         if (EntryPointCatcher.crashedDuringStartup()) {
