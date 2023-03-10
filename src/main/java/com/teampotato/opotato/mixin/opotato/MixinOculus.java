03eaffa9fae23aee1b5eb35455466fe149c69238
Kasualix <670067575@qq.com>
2023-03-10 19:25:13 +0800

    use overwrite

--- src/main/java/com/teampotato/opotato/mixin/opotato/MixinOculus.java
+++ src/main/java/com/teampotato/opotato/mixin/opotato/MixinOculus.java
@@ -2,15 +2,16 @@ package com.teampotato.opotato.mixin.opotato;
 
 import net.coderbot.iris.Iris;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = Iris.class, remap = false)
 public class MixinOculus {
-    @Inject(method = "hasNotEnoughCrashes", at = @At(value = "HEAD"), cancellable = true)
-    public static boolean removeNEC(CallbackInfo info) {
-        info.cancel();
+    /**
+     * @author Doctor Who
+     * @reason Saving The World
+     */
+    @Overwrite
+    public static boolean hasNotEnoughCrashes() {
         return false;
     }
 }
