bd5a745bb74407a7c06dba9b21f724eed3002660
Kasualix <670067575@qq.com>
2023-03-11 12:55:00 +0800

    Update PotatoCommonConfig.java

--- src/main/java/com/teampotato/opotato/config/PotatoCommonConfig.java
+++ src/main/java/com/teampotato/opotato/config/PotatoCommonConfig.java
@@ -11,12 +11,12 @@ public class PotatoCommonConfig {
     static {
         ForgeConfigSpec.Builder CONFIG_BUILDER = new ForgeConfigSpec.Builder();
         CONFIG_BUILDER.push("Opotato Common Config");
-        CONFIG_BUILDER.comment("Alternate Current");
+        CONFIG_BUILDER.comment("[Alternate Current]");
         ALTERNATE_CURRENT_DEBUG_MODE = CONFIG_BUILDER.define("enable alternate current debug mode", false);
-        CONFIG_BUILDER.comment("-----------------------------------");
 
         ENABLE_FENCE_JUMP = CONFIG_BUILDER
                 .comment(
+                        "-----------------------------------",
                         "Compared with the 'Jump Over Fences' mod, the fence jumping function of this mod is better optimized.",
                         "I(Team Potato Coder, Kasualix) had experienced crash with 'Jump Over Fences' before, so I don't want to use it again and wrote this instead.",
                         "This is disabled by default, because this is not optimization & don't fit well with the concept of Opotato, but I do need this even for my personal use.",
@@ -25,7 +25,7 @@ public class PotatoCommonConfig {
                 .define("enable fence jump", false);
         CONFIG_BUILDER.comment(
                 "-----------------------------------",
-                "Headshot",
+                "[Headshot]",
                 "This is NOT a copy of chronosacaria's Headshot-Forge",
                 "Yeah I sent a PR to improve it, but he seemed to abandon 1.16.5 Forge, so I don't want to wait for him anymore.",
                 "This version is more like the original SilverAndro's Fabric Headshot, with utils to check whether an arrow hit is on entity's head.",
