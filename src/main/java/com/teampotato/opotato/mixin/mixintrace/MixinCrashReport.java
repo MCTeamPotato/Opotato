96f8c1f56f18c6b290428fd5612c0f7f67c30181
Kasualix <670067575@qq.com>
2023-03-16 20:57:45 +0800

    fix

--- src/main/java/com/teampotato/opotato/mixin/mixintrace/MixinCrashReport.java
+++ src/main/java/com/teampotato/opotato/mixin/mixintrace/MixinCrashReport.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = CrashReport.class, remap = true)
+@Mixin(value = CrashReport.class)
 public abstract class MixinCrashReport {
     @Shadow
     private StackTraceElement[] uncategorizedStackTrace;
