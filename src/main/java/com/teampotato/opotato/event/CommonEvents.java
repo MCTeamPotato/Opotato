f4196272704ee0530ece903501f8640b7f38b99f
Kasualix <670067575@qq.com>
2023-03-22 23:15:41 +0800

    duplicate entity uuid fix

--- src/main/java/com/teampotato/opotato/event/CommonEvents.java
+++ src/main/java/com/teampotato/opotato/event/CommonEvents.java
@@ -9,11 +9,17 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Random;
+import java.util.UUID;
 
 @Mod.EventBusSubscriber
 public class CommonEvents {
@@ -45,4 +51,24 @@ public class CommonEvents {
     public static void onRegisterCommands(RegisterCommandsEvent event) {
         Opotato.OpotatoCommand.register(event.getDispatcher());
     }
+
+    private static final Random RANDOM = new Random();
+
+    @SubscribeEvent
+    public static void duplicateEntityUUIDFix(EntityJoinWorldEvent event) {
+        if (event.getWorld() instanceof ServerWorld) {
+            ServerWorld world = (ServerWorld) event.getWorld();
+            Entity entity = event.getEntity();
+            if (entity instanceof PlayerEntity) return;
+            Entity existing = world.getEntity(entity.getUUID());
+            if (existing != null && existing != entity) {
+                UUID newUUID = MathHelper.createInsecureUUID(RANDOM);
+                while (world.getEntity(newUUID) != null) {
+                    newUUID = MathHelper.createInsecureUUID(RANDOM);
+                }
+                entity.setUUID(newUUID);
+            }
+        }
+    }
+
 }
