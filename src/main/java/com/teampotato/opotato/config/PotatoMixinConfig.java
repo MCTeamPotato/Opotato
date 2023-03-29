93e55e564c8ad701c9b89c42daa54961a0296a37
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2023-03-30 00:24:55 +0800

    Update PotatoMixinConfig.java

--- src/main/java/com/teampotato/opotato/config/PotatoMixinConfig.java
+++ src/main/java/com/teampotato/opotato/config/PotatoMixinConfig.java
@@ -125,7 +125,6 @@ public class PotatoMixinConfig {
             writer.write("#\n");
             writer.write("# The following options can be enabled or disabled if there is a compatibility issue.\n");
             writer.write("# Add a line mixin.example_name=true/false without the # sign to enable/disable a rule.\n");
-            writer.write("# Only the mixin.betterbeds is false by default.\n");
             List<String> lines = this.options.keySet().stream().filter(key -> !key.equals("mixin.core")).sorted().map(key -> "#   " + key + "\n").collect(Collectors.toList());
             for(String line : lines) {
                 writer.write(line);
