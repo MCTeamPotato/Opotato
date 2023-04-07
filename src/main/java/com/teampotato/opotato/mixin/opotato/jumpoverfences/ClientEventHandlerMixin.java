a364e5953754a49f0b49fc058cbe54b77bbf1197
Kasualix <670067575@qq.com>
2023-04-07 17:28:10 +0800

    further optimize Jump Over Fences

--- src/main/java/com/teampotato/opotato/mixin/opotato/jumpoverfences/ClientEventHandlerMixin.java
+++ src/main/java/com/teampotato/opotato/mixin/opotato/jumpoverfences/ClientEventHandlerMixin.java
@@ -1,13 +1,14 @@
 package com.teampotato.opotato.mixin.opotato.jumpoverfences;
 
 import com.kreezcraft.jumpoverfences.ClientEventHandler;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.FenceBlock;
 import net.minecraft.block.WallBlock;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.stream.StreamSupport;
 
 @Mixin(value = ClientEventHandler.class, remap = false)
 public class ClientEventHandlerMixin {
@@ -18,12 +19,8 @@ public class ClientEventHandlerMixin {
     @Overwrite
     private static boolean isPlayerNextToFence(ClientPlayerEntity player) {
         BlockPos playerPos = player.blockPosition();
-        Iterable<BlockPos> positions = BlockPos.betweenClosed(playerPos.offset(-1, 0, -1), playerPos.offset(1, 0, 1));
-        for (BlockPos blockPos : positions) {
-            if (blockPos.equals(playerPos)) continue;
-            BlockState blockState = player.level.getBlockState(blockPos);
-            if (blockState.getBlock() instanceof FenceBlock || blockState.getBlock() instanceof WallBlock) return true;
-        }
-        return false;
+        return StreamSupport.stream(BlockPos.betweenClosed(playerPos.offset(-1, 0, -1), playerPos.offset(1, 0, 1)).spliterator(), false)
+                .anyMatch(blockPos -> player.level.getBlockState(blockPos).getBlock() instanceof FenceBlock ||
+                        player.level.getBlockState(blockPos).getBlock() instanceof WallBlock);
     }
 }
