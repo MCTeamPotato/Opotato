8cfee4f622f9c7bedaec7d0b03413822eb483427
Kasualix <670067575@qq.com>
2023-05-14 17:13:43 +0800

    tired

--- src/main/java/com/teampotato/opotato/config/PotatoCommonConfig.java
+++ src/main/java/com/teampotato/opotato/config/PotatoCommonConfig.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class PotatoCommonConfig {
     public static ForgeConfigSpec COMMON_CONFIG;
     public static ForgeConfigSpec.BooleanValue ALLOW_LIMIT_MAX_SPAWN, ENABLE_BLUE_SKIES_NERF, ALLOW_EVERY_MOD_GEN_FEATURE_IN_DIM, ENABLE_HEADSHOT_SOUND_DING, KILL_WITHER_STORM_MOD_ENTITIES_AFTER_COMMAND_BLOCK_DIES, LET_WITHER_SICKNESS_ONLY_TAKE_EFFECT_ON_PLAYERS, LET_MOBS_IMMUNE_TO_WITHER_SICKNESS_TICKING_SYSTEM, LET_ANIMALS_IMMUNE_TO_WITHER_SICKNESS_TICKING_SYSTEM, BLOCK_CLUSTER_RENDER_OPTIMIZATION, ENABLE_CREATIVE_ONE_POUCH, DISABLE_ARS_NOUVEAU_MANA_GEM_IN_BASIC_LOOT, IGNITIUM_ARMOR_HAS_DAMAGE, FINAL_FRACTAL_HAS_DAMAGE, INFERNAL_FORGE_HAS_DAMAGE, MONSTROUS_HELM_HAS_DAMAGE, ZWEIENDER_HAS_DAMAGE;
-    public static ForgeConfigSpec.IntValue GATE_KEEPER_HOUSE_SPACING, MAX_ENTITIES_NUMBER_PER_CHUNK, SOUL_BLACK_SMITH_SPACING, SOUL_BLACK_SMITH_SEPARATION, RUINED_CITADEL_SPACING, RUINED_CITADEL_SEPARATION, BURNING_ARENA_SPACING, BURNING_ARENA_SEPARATION, CATACOMB_SPACING, CATACOMB_SEPARATION, INCINERATOR_COOL_DOWN, INFERNAL_FORGE_COOL_DOWN, VOID_CORE_COOL_DOWN, APPRENTICE_ARMOR_MAX_MANA_BOOST, APPRENTICE_ARMOR_MANA_REGEN_BONUS, MASTER_ARMOR_MAX_MANA_BOOST, NOVICE_ARMOR_MAX_MANA_BOOST, MASTER_ARMOR_MANA_REGEN_BONUS, NOVICE_ARMOR_MANA_REGEN_BONUS, MANA_BOOST_ENCHANTMENT_MAX_LEVEL, MORIRS_DEATH_WISH_DURABILITY_AMOUNT, DURABILITY_MORIRS_DEATH_WISH_GIVE_ON_DEATH, MORIRS_DEATH_WISH_COOL_DOWN_TICKS,MORIRS_LIFE_BOUND_DURABILITY_GIVE_ON_HEAL, MORIRS_LIFE_BOUND_DURABILITY_LOST_ON_DEATH, GUIDENS_REGRET_DURABILITY_GIVE_ON_KILL, ENVIOUS_KIND_EFFECT_DURATION;
-    public static ForgeConfigSpec.ConfigValue<Float> HEADSHOT_VOLUME, HEADSHOT_PITCH, LAST_SWEET_DREAM_DISAPPEAR_PERCENT;
+    public static ForgeConfigSpec.IntValue GATE_KEEPER_HOUSE_SPACING, MAX_ENTITIES_NUMBER_PER_CHUNK, SOUL_BLACK_SMITH_SPACING, SOUL_BLACK_SMITH_SEPARATION, RUINED_CITADEL_SPACING, RUINED_CITADEL_SEPARATION, BURNING_ARENA_SPACING, BURNING_ARENA_SEPARATION, CATACOMB_SPACING, CATACOMB_SEPARATION, INCINERATOR_COOL_DOWN, INFERNAL_FORGE_COOL_DOWN, VOID_CORE_COOL_DOWN, APPRENTICE_ARMOR_MAX_MANA_BOOST, APPRENTICE_ARMOR_MANA_REGEN_BONUS, MASTER_ARMOR_MAX_MANA_BOOST, NOVICE_ARMOR_MAX_MANA_BOOST, MASTER_ARMOR_MANA_REGEN_BONUS, NOVICE_ARMOR_MANA_REGEN_BONUS, MANA_BOOST_ENCHANTMENT_MAX_LEVEL, MORIRS_DEATH_WISH_DURABILITY_AMOUNT, DURABILITY_MORIRS_DEATH_WISH_GIVE_ON_DEATH, MORIRS_DEATH_WISH_COOL_DOWN_TICKS,MORIRS_LIFE_BOUND_DURABILITY_GIVE_ON_HEAL, MORIRS_LIFE_BOUND_DURABILITY_LOST_ON_DEATH, GUIDENS_REGRET_DURABILITY_GIVE_ON_KILL, ENVIOUS_KIND_EFFECT_DURATION, EYESORE_ARROW_EXPLODE_TICKS, EYESORE_ARROW_EXPLOSION_DAMAGE, EYESORE_ARROW_BLINDNESS_DURATION, EYESORE_ARROW_SLOWNESS_DURATION, THORN_IN_FLESH_EFFECT_INTERVAL;
+    public static ForgeConfigSpec.ConfigValue<Float> HEADSHOT_VOLUME, HEADSHOT_PITCH, LAST_SWEET_DREAM_DISAPPEAR_PERCENT, THORN_IN_FLESH_DAMAGE_ON_PLAYER;
     public static ForgeConfigSpec.DoubleValue MORIRS_DEATH_WISH_MAX_DAMAGE_IN_COUNT, MORIRS_LIFE_BOUND_MAX_DAMAGE_IN_COUNT, ENVIOUS_KIND_DIFF_HEALTH;
     public static ForgeConfigSpec.ConfigValue<List<? extends String>> BLACKLIST;
 
@@ -52,13 +52,32 @@ public class PotatoCommonConfig {
         CONFIG_BUILDER.pop();
         CONFIG_BUILDER.push("Last Sweet Dream");
         LAST_SWEET_DREAM_DISAPPEAR_PERCENT = CONFIG_BUILDER
-                .define("How many percents of durability will the item with this enchantment disappear on toss", 0.1F);
+                .defineInRange("How many percents of durability will the item with this enchantment disappear on toss", 0.1F, 0.1F, 0.9F, Float.class);
         CONFIG_BUILDER.pop();
         CONFIG_BUILDER.push("Envious Kind");
         ENVIOUS_KIND_DIFF_HEALTH = CONFIG_BUILDER
                 .defineInRange("How much health will be one buff level", 10.0, 1.0, Double.MAX_VALUE);
         ENVIOUS_KIND_EFFECT_DURATION = CONFIG_BUILDER
                 .defineInRange("How long will the Envious Being effect be given (ticks)", 200, 1, Integer.MAX_VALUE);
+        CONFIG_BUILDER.pop();
+        CONFIG_BUILDER.push("Eyesore");
+        EYESORE_ARROW_EXPLODE_TICKS = CONFIG_BUILDER
+                .defineInRange("How long will the Eyesore Arrow explode since being shot out", 61, 1, Integer.MAX_VALUE);
+        EYESORE_ARROW_EXPLOSION_DAMAGE = CONFIG_BUILDER
+                .comment("The explosion calculation: basic damage + (enchantment level - 1) * 2")
+                .defineInRange("How many basic damage will the Eyesore Arrow explosion cause", 3, 1, Integer.MAX_VALUE);
+        EYESORE_ARROW_BLINDNESS_DURATION = CONFIG_BUILDER
+                .comment("Blindness length calculation: basic length + 20 * (enchantment level - 1)")
+                .defineInRange("How long will the blindness caused by the Eyesore Arrow last basically (in ticks)", 60, 1, Integer.MAX_VALUE);
+        EYESORE_ARROW_SLOWNESS_DURATION = CONFIG_BUILDER
+                .comment("Slowness length calculation: basic length + 20 * (enchantment level - 1)")
+                .defineInRange("How long will the slowness caused by the Eyesore Arrow last basically (in ticks)", 60, 1, Integer.MAX_VALUE);
+        CONFIG_BUILDER.pop();
+        CONFIG_BUILDER.push("Thorn In Flesh");
+        THORN_IN_FLESH_DAMAGE_ON_PLAYER = CONFIG_BUILDER
+                .defineInRange("How many Thorn damages Thorn In Flesh will give on player", 1.0F, 0.0F, Float.MAX_VALUE, Float.class);
+        THORN_IN_FLESH_EFFECT_INTERVAL = CONFIG_BUILDER
+                .defineInRange("Effective interval (stop moving for entities, take 1 thorn damage for players)", 50, 0, Integer.MAX_VALUE);
         CONFIG_BUILDER.pop();
         CONFIG_BUILDER.pop();
 
@@ -96,10 +115,10 @@ public class PotatoCommonConfig {
                 .define("enable 'Ding' on Headshot", true);
         HEADSHOT_VOLUME = CONFIG_BUILDER
                 .comment("Range: 0.0F ~ 1.0F. Type: Float")
-                .define("'Ding' volume", 1.0F);
+                .defineInRange("'Ding' volume", 1.0F, 0.0F, Float.MAX_VALUE, Float.class);
         HEADSHOT_PITCH = CONFIG_BUILDER
                 .comment("Range: 0.5F ~ 2.0F. Type: Float")
-                .define("'Ding' pitch", 1.0F);
+                .defineInRange("'Ding' pitch", 1.0F, 0.0F, Float.MAX_VALUE, Float.class);
         CONFIG_BUILDER.pop();
 
         CONFIG_BUILDER.push("Cracker's Wither Storm Mod Optimization Settings").comment("Every settings here will improve performance a lot as the mod is really laggy");
