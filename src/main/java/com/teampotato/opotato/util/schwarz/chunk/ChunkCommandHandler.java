d01ad3335dfb96a9eab79d11faf41beaff352415
Kasualix <670067575@qq.com>
2023-05-25 10:08:46 +0800

    Update ChunkCommandHandler.java

--- src/main/java/com/teampotato/opotato/util/schwarz/chunk/ChunkCommandHandler.java
+++ src/main/java/com/teampotato/opotato/util/schwarz/chunk/ChunkCommandHandler.java
@@ -3,7 +3,6 @@ package com.teampotato.opotato.util.schwarz.chunk;
 import com.teampotato.opotato.Opotato;
 import net.minecraft.command.CommandSource;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.chunk.Chunk;
 
 import java.util.Collections;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -12,9 +11,8 @@ public class ChunkCommandHandler {
     private static CopyOnWriteArrayList<ChunkStatus> scores = new CopyOnWriteArrayList<>();
 
     public static void analyseChunk(CommandSource serverCommandSource) {
-        CopyOnWriteArrayList<Chunk> currentWorldChunkList = Opotato.loadedChunks;
         scores.clear();
-        currentWorldChunkList.forEach(chunk -> scores.add(new ChunkStatus(chunk)));
+        Opotato.loadedChunks.forEach(chunk -> scores.add(new ChunkStatus(chunk)));
         scores.forEach(ChunkStatus::genScore);
         Collections.sort(scores);
         serverCommandSource.sendSuccess(new StringTextComponent("Chunk Analyse Report \n"), false);
