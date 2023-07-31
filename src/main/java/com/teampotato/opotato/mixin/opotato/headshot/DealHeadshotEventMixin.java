adc9de2e2ad357c26253f7cee433e5eaa387cf0d
Kasualix <670067575@qq.com>
2023-07-31 21:52:30 +0800

    fix Headshot

--- src/main/java/com/teampotato/opotato/mixin/opotato/headshot/DealHeadshotEventMixin.java
+++ src/main/java/com/teampotato/opotato/mixin/opotato/headshot/DealHeadshotEventMixin.java
@@ -1,66 +1,39 @@
 package com.teampotato.opotato.mixin.opotato.headshot;
 
-import chronosacaria.headshot.config.HeadshotConfig;
 import chronosacaria.headshot.events.DealHeadshotEvent;
 import com.teampotato.opotato.config.PotatoCommonConfig;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.DamageSource;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = DealHeadshotEvent.class, remap = false)
 public abstract class DealHeadshotEventMixin {
-    /**
-     * @author Kasualix
-     * @reason Optimize headshot
-     */
-    @Overwrite
-    @SubscribeEvent
-    public static void onHeadshotIfApplicable(LivingDamageEvent event) {
-        LivingEntity damaged = event.getEntityLiving();
-        World world = damaged.level;
-        DamageSource eventSource = event.getSource();
-        Vector3d srcPos = eventSource.getSourcePosition();
-        if (world.isClientSide || !eventSource.isProjectile() || srcPos == null ||
-                damaged.isInvulnerableTo(eventSource) ||
-                srcPos.y <= damaged.position().add(0.0, 0.85 * (double) damaged.getDimensionsForge(damaged.getPose()).height, 0.0).y - 0.17 ||
-                !(!HeadshotConfig.HELMET_MITIGATION.get() || damaged.getItemBySlot(EquipmentSlotType.HEAD).isEmpty())
-        ) return;
+    @Inject(method = "onHeadshotIfApplicable", at = @At("HEAD"), cancellable = true)
+    private static void onHeadshot(LivingDamageEvent event, CallbackInfo ci) {
+        if (event.getSource().getSourcePosition() == null) ci.cancel();
+    }
 
-        PlayerEntity player = (damaged instanceof PlayerEntity) ? (PlayerEntity) damaged : null;
-        if (player != null) player.displayClientMessage(new TranslationTextComponent("headshot.opotato.on_player"), true);
+    @Inject(method = "onHeadshotIfApplicable", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/ServerPlayerEntity;displayClientMessage(Lnet/minecraft/util/text/ITextComponent;Z)V", shift = At.Shift.BEFORE))
+    private static void playSound(LivingDamageEvent event, CallbackInfo ci) {
+        if (PotatoCommonConfig.ENABLE_HEADSHOT_SOUND_DING.get() && event.getEntityLiving() instanceof PlayerEntity)
+            event.getEntityLiving().playSound(SoundEvents.ARROW_HIT_PLAYER, PotatoCommonConfig.HEADSHOT_VOLUME.get(), PotatoCommonConfig.HEADSHOT_PITCH.get());
+    }
 
-        Entity trueSource = eventSource.getEntity();
-        PlayerEntity sourcePlayer = (trueSource instanceof PlayerEntity) ? (PlayerEntity) trueSource : null;
-        if (sourcePlayer != null) sourcePlayer.displayClientMessage(new TranslationTextComponent("headshot.opotato.on_entity"), true);
+    @Redirect(method = "onHeadshotIfApplicable", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/ServerPlayerEntity;displayClientMessage(Lnet/minecraft/util/text/ITextComponent;Z)V", ordinal = 0))
+    private static void onDisplayMessage1(ServerPlayerEntity instance, ITextComponent pChatComponent, boolean pActionBar) {
+        instance.displayClientMessage(new TranslationTextComponent("headshot.opotato.on_player"), true);
+    }
 
-        double headshotDamage = HeadshotConfig.HEADSHOT_DAMAGE_MULTIPLIER.get() * ((double)event.getAmount());
-        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.PROJECTILE_PROTECTION, damaged) > 0) {
-            event.setAmount((float) (headshotDamage * HeadshotConfig.HEADSHOT_PROJECTILE_PROTECTION_DAMAGE_REDUCTION.get()));
-            damaged.getItemBySlot(EquipmentSlotType.HEAD).hurt((int) (headshotDamage / 4), world.random, null);
-        } else {
-            event.setAmount((float) headshotDamage);
-            damaged.getItemBySlot(EquipmentSlotType.HEAD).hurt((int) (headshotDamage / 2), world.random, null);
-
-            if (HeadshotConfig.DO_BLINDNESS.get())
-                damaged.addEffect(new EffectInstance(Effects.BLINDNESS, HeadshotConfig.BLIND_TICKS.get(), 3));
-            if (HeadshotConfig.DO_NAUSEA.get())
-                damaged.addEffect(new EffectInstance(Effects.CONFUSION, HeadshotConfig.NAUSEA_TICKS.get(), 2));
-        }
-        if (PotatoCommonConfig.ENABLE_HEADSHOT_SOUND_DING.get() && damaged instanceof PlayerEntity)
-            damaged.playSound(SoundEvents.ARROW_HIT_PLAYER, PotatoCommonConfig.HEADSHOT_VOLUME.get(), PotatoCommonConfig.HEADSHOT_PITCH.get());
+    @Redirect(method = "onHeadshotIfApplicable", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/ServerPlayerEntity;displayClientMessage(Lnet/minecraft/util/text/ITextComponent;Z)V", ordinal = 1))
+    private static void onDisplayMessage2(ServerPlayerEntity instance, ITextComponent pChatComponent, boolean pActionBar) {
+        instance.displayClientMessage(new TranslationTextComponent("headshot.opotato.on_entity"), true);
     }
 }
