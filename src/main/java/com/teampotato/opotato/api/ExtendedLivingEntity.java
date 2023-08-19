bb5373c3f303797c70f6ed808cde2dd489eafbaa
Kasualix <670067575@qq.com>
2023-08-20 01:37:45 +0800

    fix

---
+++ src/main/java/com/teampotato/opotato/api/ExtendedLivingEntity.java
@@ -0,0 +1,7 @@
+package com.teampotato.opotato.api;
+
+import net.minecraft.util.DamageSource;
+
+public interface ExtendedLivingEntity {
+    boolean opotato$isDamageSourceBlocked(DamageSource pDamageSource);
+}
