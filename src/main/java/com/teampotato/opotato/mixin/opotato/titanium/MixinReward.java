108293d15e15815862b2e1a4274433b7e5aca035
Kasualix <670067575@qq.com>
2023-12-16 18:14:48 +0800

    fix titanium mixin

--- src/main/java/com/teampotato/opotato/mixin/opotato/titanium/MixinReward.java
+++ src/main/java/com/teampotato/opotato/mixin/opotato/titanium/MixinReward.java
@@ -1,6 +1,5 @@
 package com.teampotato.opotato.mixin.opotato.titanium;
 
-import com.google.common.collect.Lists;
 import com.hrznstudio.titanium.reward.Reward;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +16,6 @@ import java.util.UUID;
 public abstract class MixinReward {
     @Inject(method = "getPlayers(Ljava/net/URL;)Ljava/util/List;", at = @At("HEAD"), cancellable = true)
     private static void onGetPlayers(URL url, @NotNull CallbackInfoReturnable<List<UUID>> cir) {
-        cir.setReturnValue(Lists.newArrayList());
+        cir.setReturnValue(Collections.emptyList());
     }
 }
---
+++ src/main/java/com/teampotato/opotato/mixin/opotato/titanium/MixinRewardGiver.java
@@ -0,0 +1,16 @@
+package com.teampotato.opotato.mixin.opotato.titanium;
+
+import com.hrznstudio.titanium.reward.Reward;
+import com.hrznstudio.titanium.reward.RewardGiver;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RewardGiver.class)
+public abstract class MixinRewardGiver {
+    @Inject(method = "addReward", at = @At("HEAD"), cancellable = true)
+    private void onAddReward(Reward reward, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
