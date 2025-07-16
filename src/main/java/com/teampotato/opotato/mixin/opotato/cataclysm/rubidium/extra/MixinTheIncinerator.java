559284d8092a5c505315898f68d414707ad13762
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-16 17:52:48 +0800

    fix crash on server side

--- src/main/java/com/teampotato/opotato/mixin/opotato/cataclysm/rubidium/extra/MixinTheIncinerator.java
+++ src/main/java/com/teampotato/opotato/mixin/opotato/cataclysm/rubidium/extra/MixinTheIncinerator.java
@@ -4,10 +4,8 @@ import L_Ender.cataclysm.items.The_Incinerator;
 import com.teampotato.opotato.EarlySetupInitializer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,10 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinTheIncinerator {
     @Inject(method = "onUsingTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;playSound(Lnet/minecraft/sounds/SoundEvent;FF)V", shift = At.Shift.AFTER))
     private void fogHint(ItemStack stack, @NotNull LivingEntity livingEntity, int count, CallbackInfo ci) {
-        ServerPlayer serverPlayer = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayer(livingEntity.getUUID());
-        if (livingEntity instanceof LocalPlayer && serverPlayer != null && !serverPlayer.getTags().contains(EarlySetupInitializer.MOD_ID + ".sodiumExtra.fogHint")) {
-            ((LocalPlayer)livingEntity).displayClientMessage(new TranslatableComponent("opotato.catatclysm.incinerator.fogHint"), false);
-            serverPlayer.addTag(EarlySetupInitializer.MOD_ID + ".sodiumExtra.fogHint");
+        if (livingEntity instanceof LocalPlayer) {
+            LocalPlayer player = (LocalPlayer) livingEntity;
+            if (!player.getPersistentData().getBoolean(EarlySetupInitializer.MOD_ID + ".sodiumExtra.fogHint")) {
+                player.getPersistentData().putBoolean(EarlySetupInitializer.MOD_ID + ".sodiumExtra.fogHint", true);
+                player.displayClientMessage(new TranslatableComponent("opotato.catatclysm.incinerator.fogHint"), false);
+            }
         }
     }
 }
